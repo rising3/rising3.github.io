88c249e483e0a7537123eeec3c63594b515a24a2
rising3 <michio.nakagawa@gmail.com>
2024-02-25 00:58:41 +0900

    fix: contents

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,18 @@
 import { z, defineCollection } from "astro:content";
+
+const projectSchema = z.object({
+    title: z.string(),
+    description: z.string(),
+    url: z.string(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.string().optional(),
+    heroImage: z.string().optional(),
+    badge: z.string().optional(),
+    tags: z.array(z.string()).refine(items => new Set(items).size === items.length, {
+        message: 'tags must be unique',
+    }).optional(),
+});
+
 const blogSchema = z.object({
     title: z.string(),
     description: z.string(),
@@ -24,13 +38,16 @@ const storeSchema = z.object({
     heroImage: z.string().optional(),
 });
 
+export type ProjectSchema = z.infer<typeof projectSchema>;
 export type BlogSchema = z.infer<typeof blogSchema>;
 export type StoreSchema = z.infer<typeof storeSchema>;
 
-const blogCollection = defineCollection({ schema: blogSchema });
-const storeCollection = defineCollection({ schema: storeSchema });
+const projectCollection = defineCollection({ type: 'data', schema: projectSchema });
+const blogCollection = defineCollection({  type: 'content', schema: blogSchema });
+const storeCollection = defineCollection({  type: 'content', schema: storeSchema });
 
 export const collections = {
+    'project': projectCollection,
     'blog': blogCollection,
     'store': storeCollection
 }
---
+++ src/content/store/empty.md
@@ -0,0 +1,11 @@
+---
+title: ""
+description: ""
+custom_link_label: "Live Preview"
+custom_link: "https://demourl.com/"
+updatedDate: "Sep 15 2022"
+pricing: "$15"
+badge: "Featured"
+checkoutUrl: "https://checkouturl.com/"
+heroImage: "/itemPreview.webp"
+---
